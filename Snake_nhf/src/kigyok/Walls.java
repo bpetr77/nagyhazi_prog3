7e3e2f9afe34199c3ec35c802e48222cfa0f2257
Bakó Péter <malom.tg@gmail.com>
2023-11-26 00:19:01 +0100

    walls

--- Snake_nhf/src/kigyok/Walls.java
+++ Snake_nhf/src/kigyok/Walls.java
@@ -16,20 +16,28 @@ public class Walls {
 		return walls;
 	}
 	
+	/**
+	 * Falak inicializálása véletlenszerűen a kígyó fejének elkerülésével.
+	 * A falak 0.01 valószínűséggel jönnek létre minden mezőnél, kivéve a kígyó kezdeti pozícióját.
+	 */
 	public void initwalls() {
 	        Random random = new Random();
 	        for (int row = 0; row < SnakeGame.SCREEN_HEIGHT / SnakeGame.UNIT_SIZE; row++) {
 	            for (int col = 0; col < SnakeGame.SCREEN_WIDTH / SnakeGame.UNIT_SIZE; col++) {
+	            	// A kígyó kezdeti pozíciójánál nincs fal
 	            	if(col != SnakeGame.SCREEN_WIDTH / SnakeGame.UNIT_SIZE / 2 || row != SnakeGame.SCREEN_HEIGHT / SnakeGame.UNIT_SIZE / 2) {
-	                if (random.nextDouble() < 0.01) {
-                		Point point = new Point((col * SnakeGame.UNIT_SIZE), (row * SnakeGame.UNIT_SIZE));
-                		walls.add(point);
+	            		if (random.nextDouble() < 0.01) {
+	            			Point point = new Point((col * SnakeGame.UNIT_SIZE), (row * SnakeGame.UNIT_SIZE));
+	            			walls.add(point);
                 	}
             	}
             }
         }
     }
 	
+	/**
+	 * Ellenőrzi, hogy a megadott pont(gyümölcs) ütközik-e valamely fallal.
+	 */
 	public boolean Fruitcollide(Point point) {
         for (Point wall : walls) {
             if (wall.equals(point)) {
@@ -39,6 +47,7 @@ public class Walls {
         return false;
     }
 	
+	//fal kirajzolása
 	public void draw(Graphics g) {
 		for (Point p : walls) {
 				g.setColor(new Color(61, 35, 0));
@@ -46,6 +55,7 @@ public class Walls {
 		}
 	}
 	
+	//Visszaadja a falak listájában az adott indexen található falpontot.
 	public Point getwallidx(int idx) {
 		if (idx < walls.size()) {
 			return walls.get(idx);
